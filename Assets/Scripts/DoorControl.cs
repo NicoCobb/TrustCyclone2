0589251f2a83910699a43320315b6d1d69224355
EricRFranco <ericrfranco219@gmail.com>
2017-02-27 20:41:56 -0500

    Level 1 complete

---
+++ Assets/Scripts/DoorControl.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorControl : RaycastController {
+
+	Animator anim;
+
+	static public bool open = false;
+	public Vector2 doorBoxSize;
+	public LayerMask doorMask;
+
+	// Use this for initialization
+	void Start () {
+		base.Start ();
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		UpdateRaycastOrigins();
+		CheckPlateCondition();
+		if (open) {
+			anim.SetBool ("isOpen", true);
+		} else
+			anim.SetBool ("isOpen", false);
+	}
+
+	void CheckPlateCondition()
+	{
+		Vector2 rayOrigin = raycastOrigins.center;
+		Collider2D collider = Physics2D.OverlapBox(rayOrigin, doorBoxSize, 0f, doorMask);
+
+		if (collider)
+		{
+			if (open) {
+				SceneManager.LoadScene (this.gameObject.scene.buildIndex + 1);
+			}
+		}
+			
+	}
+
+	void OnDrawGizmos()
+	{
+		Vector2 rayOrigin = raycastOrigins.center;
+
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireCube(rayOrigin, new Vector3(doorBoxSize.x, doorBoxSize.y, 0f));
+	}
+
+}
---
+++ Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : RaycastController {
+
+    Animator anim;
+
+    public Vector2 plateBoxSize;
+    public LayerMask plateMask;
+
+    // Use this for initialization
+    public override void Start () {
+        base.Start();
+        anim = GetComponent<Animator>();
+        anim.SetBool("isPressed", false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        UpdateRaycastOrigins();
+        CheckPlateCondition();
+	}
+
+    void CheckPlateCondition()
+    {
+        Vector2 rayOrigin = raycastOrigins.center;
+        Collider2D collider = Physics2D.OverlapBox(rayOrigin, plateBoxSize, 0f, plateMask);
+
+        if (collider)
+        {
+            anim.SetBool("isPressed", true);
+
+			if (tag == "DoorPlate") {
+				DoorControl.open = true;
+			}
+        }
+
+        if(!collider)
+        {
+			anim.SetBool("isPressed", false);
+
+			if (tag == "DoorPlate") {
+				DoorControl.open = false;
+			}
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Vector2 rayOrigin = raycastOrigins.center;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(rayOrigin, new Vector3(plateBoxSize.x, plateBoxSize.y, 0f));
+    }
+
+
+}
